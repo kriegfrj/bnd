10078a0c695d24fdd4419ab231b594f20e827969
Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>
2020-05-18 09:43:06 +0930

    WIP

--- bndtools.core.test/src/bndtools/core/test/Application.java
+++ bndtools.core.test/src/bndtools/core/test/Application.java
@@ -1,9 +1,10 @@
 package bndtools.core.test;
 
-import org.eclipse.e4.ui.internal.workbench.swt.E4Application;
+//import org.eclipse.e4.ui.internal.workbench.swt.E4Application;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.ui.internal.ide.application.IDEApplication;
 
-public class Application extends E4Application {
+public class Application extends IDEApplication {
 
 	@Override
 	public Object start(IApplicationContext applicationContext) throws Exception {
