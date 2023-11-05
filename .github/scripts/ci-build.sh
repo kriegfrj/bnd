af2509cef008a08fc2eb8aad05eda694f1607ecf
Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>
2023-11-06 10:05:05 +1030

    [bndtools test] Shortcuts to test integration tests in CI

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -2,6 +2,6 @@
 set -ev
 ./gradlew --no-daemon --version
 ./mvnw --version
-./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :build "$@"
-./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :gradle-plugins:build
-./mvnw -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
+./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :bndtools.core.test:testOSGi
+#./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :gradle-plugins:build
+#./mvnw -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
